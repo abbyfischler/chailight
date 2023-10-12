9ed49cf4bb7c1c0ab26486285c409c0cb606bd91
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2023-10-11 21:56:28 -0700

    Formatted website

--- components/nav.js
+++ components/nav.js
@@ -16,7 +16,12 @@ export default function Nav() {
     return (
         <nav className="dark:text-white">
             <ul className="flex flex-wrap sm:justify-between items-start sm:items-center p-8 mt-6 sm:mt-0">
-                <li>Perfect Next.js Theme</li>
+             
+                <li>
+            <Link href="/">
+              <img src="/logo.png" className="w-28 h-28"></img>
+            </Link>
+          </li>
                 <ul className={`mx-auto sm:mx-0 flex flex-row space-x-5`}>
                     {links.map(({ href, label }) => (
                         <li className="self-center" key={`${href}${label}`}>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import Link from "next/link";
 import axios from 'axios'
 import useSWR from 'swr'
 import fetcher from '../lib/fetcher'
@@ -11,21 +12,25 @@ export default function Home() {
   }
   const names = useSWR('/api/all', fetcher).data;
   return (
-    <div className="dark:text-white dark:bg-black">
+    <div className="dark:bg-black text-black ">
       <main>
-       
+      <Link href="/">
+              <img src="/logo.png" className="w-16 s:w-20 md:w-32 lg:w-48 mx-auto pt-4"></img>
+            </Link>
         <section className="w-1/2 mx-auto bg-gray-200 rounded-xl p-10 mt-16">
         <p className='text-blue-700'>
 In order to hold our institutions accountable, we need your help. Please upload a screenshot of an email from your institution that demonstrates their response to antisemitism. We will use this information to create a report that will expose any institutions that are failing to live up to their responsibility. It is important for us not to stay silent.
 </p>
 <br></br>
+
+
           <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-5">
-          <label htmlFor="institution_name">Institution Name (required)</label>
-            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="name" placeholder="Harvard" {...register("institution_name", { required: true })} />
-            <label htmlFor="`institution_picture`">Please upload a picture of the Institution's response.</label>
-            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="file" id="institution_picture" {...register("institution_picture", { required: true })} />
+          <label htmlFor="institution_name" className='font-bold '>Institution Name (required)</label>
+            <input className="bg-gray-200 border-b-2 font-bold border-gray-400 focus-within:outline-none focus-within:border-gray-600 text-black" type="text" id="name" placeholder="Harvard" {...register("institution_name", { required: true })} />
+            <label htmlFor="`institution_picture`" className='font-bold '>Please upload a picture of the Institution's response.</label>
+            <input className="text-black bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="file" id="institution_picture" {...register("institution_picture", { required: true })} />
            
-            <input type="submit" className="bg-blue-500 text-white py-2 hover:bg-blue-600 rounded-xl font-bold" value="Submit"/>
+            <input type="submit" className="bg-blue-500  py-2 hover:bg-blue-600 rounded-xl" value="Submit"/>
           </form>
         </section>
         {/* <section className="flex flex-col bg-gray-200 w-1/2 mx-auto rounded-xl p-10 mt-16">
